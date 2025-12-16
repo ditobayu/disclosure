9662686c8bc2aac75d7b51c293060a4291d83468
Dito Bayu Adhitya <ditobayuadhitya@Ditos-MacBook-Air.local>
2025-12-16 10:10:16 +0700

    feat: auto commit

--- direct-disclosure.page.gd/htdocs/config.php
+++ direct-disclosure.page.gd/htdocs/config.php
@@ -2,10 +2,10 @@
 // config.php
 session_start();
 
-$DB_HOST = 'sql204.infinityfree.com';
-$DB_NAME = 'if0_40090928_penilaian_akademik';
-$DB_USER = 'if0_40090928';
-$DB_PASS = 'Nazwanaz22'; // isi sesuai environment
+$DB_HOST = 'db';
+$DB_NAME = 'penilaian_akademik';
+$DB_USER = 'user';
+$DB_PASS = 'password'; // isi sesuai environment
 
 try {
     $pdo = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME};charset=utf8mb4", $DB_USER, $DB_PASS, [
---
+++ push-to-remote.sh
@@ -0,0 +1,8 @@
+echo "Starting push to..."
+git add .
+
+echo "Committing changes..."
+git commit -m "feat: auto commit"
+
+echo "Pushing to branch..."
+git push
